1c1dfdd0d3ac4b583ab9b076b1253271fa8982e3
Jose Daniel Subias Sarrato <dsubias@hermes2.i3a.es>
2023-03-21 11:44:17 +0100

    some changes

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument(
         '--config',
-        default='configs/inference_glossy.yaml',
+        default='configs/train_model.yaml',
         help='The path of configuration file in yaml format')
     args = arg_parser.parse_args()
     config = process_config(args.config)
