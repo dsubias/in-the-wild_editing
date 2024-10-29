503060d6d021961727f8b49cc5a918b3508a95b5
José Daniel Subías <dsubias@unizar.es>
2024-10-29 15:44:42 +0100

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -3,5 +3,5 @@
 if [ ! -d ./pretrained_models ]; then
   mkdir ./pretrained_models
 fi
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1pXggdRjXVKgKF96ax2xeC-Yw4s5DxgAv' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1pXggdRjXVKgKF96ax2xeC-Yw4s5DxgAv" -O pretrained_models.zip && rm -rf /tmp/cookies.txt
+wget -O pretrained_models.zip "https://nas-graphics.unizar.es/s/2ZDbY67oTyJLPKo/download/pretrained_models.zip" 
 unzip pretrained_models.zip -d ./pretrained_models
